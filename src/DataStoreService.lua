49b825c01d51eb35052af67539450b3cc4f2fdfa
buildthomas <40648899+buildthomas@users.noreply.github.com>
2018-06-28 17:30:56 +0200

    Update DataStoreService.lua

--- src/DataStoreService.lua
+++ src/DataStoreService.lua
@@ -8,8 +8,9 @@
 	Copyright 2018 buildthomas
 ]]
 
-local shouldUseMock = false
+local MockDataStoreServiceModule = script.Parent.MockDataStoreService -- set this as path to the module
 
+local shouldUseMock = false
 if game.GameId == 0 then
 	-- Local place file, use mock:
 	shouldUseMock = true
@@ -19,7 +20,7 @@ elseif game:GetService("RunService"):IsStudio() then
 		-- This will error if current instance has no Studio API access:
 		game:GetService("DataStoreService"):GetDataStore("__TEST"):UpdateAsync("__TEST", function(...) return ... end)
 	end)
-	if not status and (message:lower():find("api access") or message:lower():find("http 403")) then
+	if not status and (message:lower():find("api access") or message:lower():find("http 403")) then -- hack
 		-- Can connect to datastores, but no API access, so use mock:
 		shouldUseMock = true
 	end
@@ -27,8 +28,8 @@ end
 
 -- Return the mock or actual service depending on environment:
 if shouldUseMock then
-	print("INFO: Using MockDataStoreService")
-	return require(script.Parent.MockDataStoreService)
+	warn("INFO: Using MockDataStoreService instead of DataStoreService")
+	return require(MockDataStoreServiceModule)
 else
 	return game:GetService("DataStoreService")
 end
