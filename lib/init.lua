9fac4109abfdf05d02a98e63e7866b5bb8376110
buildthomas <40648899+buildthomas@users.noreply.github.com>
2018-10-04 01:15:19 +0200

    Update check

--- lib/init.lua
+++ lib/init.lua
@@ -15,7 +15,7 @@ elseif game:GetService("RunService"):IsStudio() then -- Published file in Studio
 		-- This will error if current instance has no Studio API access:
 		game:GetService("DataStoreService"):GetDataStore("__TEST"):UpdateAsync("__TEST", function(...) return ... end)
 	end)
-	if not status and (message:lower():find("api access") or message:lower():find("http 403")) then -- HACK
+	if not status and message:find("403") then -- HACK
 		-- Can connect to datastores, but no API access
 		shouldUseMock = true
 	end
