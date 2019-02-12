66cec6a140d51e0bcd6ee8a05fc644a76ea87159
Thomas <buildthomas@hotmail.com>
2019-02-12 21:51:23 +0100

    Retry space fix

--- lib/MockDataStoreService/MockDataStoreConstants.lua
+++ lib/MockDataStoreService/MockDataStoreConstants.lua
@@ -11,34 +11,34 @@ return {
     LOGGING_ENABLED = false;        -- Verbose logging of transactions to output
     LOGGING_FUNCTION = warn;        -- Function for logging messages
 
-    MAX_LENGTH_KEY = 49;			-- Max number of chars in key string
-    MAX_LENGTH_NAME = 50;			-- Max number of chars in name string
-    MAX_LENGTH_SCOPE = 50;			-- Max number of chars in scope string
-    MAX_LENGTH_DATA = 260e3;		-- Max number of chars in (encoded) data strings
+    MAX_LENGTH_KEY = 49;            -- Max number of chars in key string
+    MAX_LENGTH_NAME = 50;           -- Max number of chars in name string
+    MAX_LENGTH_SCOPE = 50;          -- Max number of chars in scope string
+    MAX_LENGTH_DATA = 260e3;        -- Max number of chars in (encoded) data strings
 
-    MAX_PAGE_SIZE = 100;			-- Max page size for GetSortedAsync
+    MAX_PAGE_SIZE = 100;            -- Max page size for GetSortedAsync
 
-    YIELD_TIME_MIN = 0.2;			-- Random yield time values for set/get/update/remove/getsorted
+    YIELD_TIME_MIN = 0.2;           -- Random yield time values for set/get/update/remove/getsorted
     YIELD_TIME_MAX = 0.5;
 
-    YIELD_TIME_UPDATE_MIN = 0.2;	-- Random yield times from events from OnUpdate
+    YIELD_TIME_UPDATE_MIN = 0.2;    -- Random yield times from events from OnUpdate
     YIELD_TIME_UPDATE_MAX = 0.5;
 
-    WRITE_COOLDOWN = 6.0;			-- Amount of cooldown time between writes on the same key in a particular datastore
+    WRITE_COOLDOWN = 6.0;           -- Amount of cooldown time between writes on the same key in a particular datastore
 
-    GET_COOLDOWN = 5.0;		    	-- Amount of cooldown time that a recent interaction with a key is considered fresh
+    GET_COOLDOWN = 5.0;             -- Amount of cooldown time that a recent interaction with a key is considered fresh
 
-    THROTTLE_QUEUE_SIZE = 30;		-- Amount of requests that can be throttled at once (additional requests will error)
+    THROTTLE_QUEUE_SIZE = 30;       -- Amount of requests that can be throttled at once (additional requests will error)
 
     SIMULATE_ERROR_RATE = 0;        -- Rate at which requests will throw errors for testing (0 = never, 1 = always)
 
-    BUDGETING_ENABLED = true;		-- Whether budgets are enforced and calculated
+    BUDGETING_ENABLED = true;       -- Whether budgets are enforced and calculated
 
-    BUDGET_GETASYNC = {				-- Budget constant storing structure
-        START = 100;			    	-- Starting budget
-        RATE = 60;				    	-- Added budget per minute
-        RATE_PLR = 10;			    	-- Additional added budget per minute per player
-        MAX_FACTOR = 3;			    	-- The maximum budget as a factor of (rate + rate_plr * #players)
+    BUDGET_GETASYNC = {             -- Budget constant storing structure
+        START = 100;                    -- Starting budget
+        RATE = 60;                      -- Added budget per minute
+        RATE_PLR = 10;                  -- Additional added budget per minute per player
+        MAX_FACTOR = 3;                 -- The maximum budget as a factor of (rate + rate_plr * #players)
     };
 
     BUDGET_GETSORTEDASYNC = {
@@ -72,6 +72,6 @@ return {
     BUDGET_BASE = 60;               -- Modifiers used for budget increases on OnClose
     BUDGET_ONCLOSE_BASE = 150;
 
-    BUDGET_UPDATE_INTERVAL = 1.0;	-- Time interval in seconds at which budgets are updated (do not put too low)
+    BUDGET_UPDATE_INTERVAL = 1.0;   -- Time interval in seconds at which budgets are updated (do not put too low)
 
 }
